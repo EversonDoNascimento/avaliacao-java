e9010381e1da9f4850bf03e7bf9ca59e6761aa70
Everson do Nascimento Silva <101950445+EversonDoNascimento@users.noreply.github.com>
2022-03-21 20:58:47 -0300

    Add files via upload

---
+++ src/ArrayBidimensionalSubtracao.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class ArrayBidimensionalSubtracao {
+	
+	public static void main(String[] args) {
+
+		Random random = new Random();
+	    System.out.println(random);
+	};
+
+}
